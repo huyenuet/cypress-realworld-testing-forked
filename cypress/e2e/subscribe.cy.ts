586fa1188b59b2a4362780f5e8fec92cbef63b69
huyen tran <thanhhuyenk59uet@gmail.com>
2023-12-04 22:01:47 +0700

    feat: play with alias

--- cypress/e2e/subscribe.cy.ts
+++ cypress/e2e/subscribe.cy.ts
@@ -1,8 +1,12 @@
-describe("Newsletter subscribe form", () => {
-    beforeEach(() => {
-        cy.visit("http://localhost:3000")
+describe("Newsletter subscribe form", function() {
+    beforeEach(function() {
+        cy.visit("")
+        cy.get('button').first().invoke('text').as("button_text")
     })
-    it("allow user to subscribe to email list", () => {
+    it("allow user to subscribe to email list", function() {
+        cy.get('@button_text').then(button_text => {
+            expect(button_text).to.eql("Open menu")
+        })
         cy.getByData('email-input')
         .type("smartmouse.fun@gmail.com")
         cy.getByData('submit-button').click()
@@ -10,6 +14,7 @@ describe("Newsletter subscribe form", () => {
         .contains("smartmouse.fun@gmail.com")
     })
     it("does NOT allow a invalid email address", () => {
+        cy.get('@button_text').should('equal', "Open menu")
         cy.getByData('email-input').type("@gmail.com")
         cy.getByData('submit-button').click()
         cy.getByData('success-message').should('not.exist')
